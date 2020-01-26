9a0111abc00b0d591e827799240955987decbfab
DrSeraf <58887416+DrSeraf@users.noreply.github.com>
2020-01-26 13:22:22 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,15 @@
+TOKEN = 'NjY5OTU5MDEyMDIzOTI2Nzg3.XinadA.9-WUtmhW7lmLRzk8L6MtrKpWSHM' #bot token
+
+channel_id = 669977389161512983 #post id to read reactoins from
+
+#roles list according to emotes
+ROLES = {
+	'🔫': 662253296140025856, #Сигнифер
+	'👍': 661505982379327489, #Легионер
+}
+
+#exclude this roles from counting
+EXCROLES = ()
+
+#max ammount of roles a user can have
+MAX_ROLES_PER_USER = 10
