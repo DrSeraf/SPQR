e0d794dc20c776fddd37b6ab67bdc12f4282ea51
DrSeraf <58887416+DrSeraf@users.noreply.github.com>
2020-01-26 13:24:01 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 TOKEN = 'NjY5OTU5MDEyMDIzOTI2Nzg3.XinadA.9-WUtmhW7lmLRzk8L6MtrKpWSHM' #bot token
 
-channel_id = 669977389161512983 #post id to read reactoins from
+channel_id = 670688463317630991 #post id to read reactoins from
 
 #roles list according to emotes
 ROLES = {
